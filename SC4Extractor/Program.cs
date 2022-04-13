39ea20067bbb5fc9380deb894e6e4eacbedf72b5
Matthew Carney <matthewcarney64@gmail.com>
2022-04-13 22:11:14 +0100

    Initial commit

---
+++ SC4Extractor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using SC4Parser.Files;
+
+namespace SC4Extractor // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SC4SaveFile file = new SC4SaveFile(@"C:\Users\Shadowfax\Downloads\City_-_Leptonm.sc4"))
+            {
+                //file.Dump();
+                file.GetNetworkSubfile1().Dump();
+            }
+        }
+    }
+}

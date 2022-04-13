f076c8419529bb685e74930c169eeb80c3d6d19a
Matthew Carney <matthewcarney64@gmail.com>
2022-04-13 22:41:46 +0100

    Added NetworkIndex file parsing

--- SC4Extractor/Program.cs
+++ SC4Extractor/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 
+using SC4Parser;
+using SC4Parser.Subfiles;
 using SC4Parser.Files;
+using SC4Parser.DataStructures;
 
 namespace SC4Extractor // Note: actual namespace depends on the project name.
 {
@@ -8,10 +11,15 @@ namespace SC4Extractor // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            using (SC4SaveFile file = new SC4SaveFile(@"C:\Users\Shadowfax\Downloads\City_-_Leptonm.sc4"))
+            using (SC4SaveFile file = new(@"C:\Users\Shadowfax\Downloads\City_-_Leptonm.sc4"))
             {
-                //file.Dump();
-                file.GetNetworkSubfile1().Dump();
+                IndexEntry networkIndex = file.FindIndexEntryWithType(Constants.NETWORK_INDEX_SUBFILE_TYPE); // TODO Add load with type to parser
+                byte[] indexFileData = file.LoadIndexEntry(networkIndex);
+
+                NetworkIndex networkFile = new();
+                networkFile.Parse(indexFileData);
+                networkFile.Dump();
+                
             }
         }
     }
